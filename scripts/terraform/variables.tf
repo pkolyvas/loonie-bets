0057088a8a243d6d64fe315732b9db961af3686e
Petros Kolyvas <code@petros.io>
2022-06-23 11:42:58 -0300

    Comment

--- scripts/terraform/variables.tf
+++ scripts/terraform/variables.tf
@@ -1,3 +1,5 @@
+# We expect this variable to be set by a TF_VAR environment variable derived from the packer AMI, but a datasource is
+# probably a better way to go and eventually how we'd do it.
 variable "packer-loonie" {
   type        = string
   description = "The AMI used for our EC2 instance"
